c12f61d678b4bff3fc9cbfceeec4318eb588d019
jac0320 <sitew@clemson.edu>
2015-11-18 00:20:02 -0500

    Some basic things I can think of for the moment. We don't have to stick to it. Have a good night, guys.

---
+++ main.py
@@ -0,0 +1,85 @@
+
+class tree(object):
+    def __init__(self):
+        #TODO
+        pass
+
+class dict_tree(object):
+    def __init__(self):
+        #TODO
+        pass
+class heap_tree(object):
+    def __init__(self):
+        #TODO
+        pass
+
+class rbTree(tree):
+    """
+        #TODO:Description
+    """
+    def __init__(self):
+        #TODO
+        pass
+
+    def _find_parent(self,x):
+        #TODO
+        pass
+
+    def _go_left(self,x):
+        #TODO
+        pass
+
+    def _go_right(self,x):
+        #TODO
+        pass
+
+    def _find_uncle(self,x):
+        #TODO
+        pass
+
+    def _find_grandparent(self,x):
+        #TODO
+        pass
+
+    def _color_red(self,x):
+        #TODO
+        pass
+
+    def _color_black(self,x):
+        #TODO
+        pass
+
+    def _rotate_left(self,x):
+        #TODO
+        pass
+
+    def _rotate_right(self,x):
+        #TODO
+        pass
+
+    def _property_check(self):
+        #TODO
+        pass
+
+    def insertion( self, x ) :
+        #TODO
+        pass
+
+    def deletion (self,x):
+        #TODO
+        pass
+
+    def depth(self):
+        #TODO
+        pass
+
+    def is_empty(self):
+        #TODO
+        pass
+
+    def print_tree(self):
+        #TODO
+        pass
+
+class test()
+    pass
