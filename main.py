540f746c713c74301f07a25411515fb35f4b92fa
jac0320 <sitew@clemson.edu>
2015-11-20 17:14:19 -0500

    Implemented basic ploting technique. Color is changeable. Haven't tested yet on mac. Tested on Sagemath. Also implemented rotate_left abd rotate_right in tree node class for re-balancing. Not sure if it will work. Haven't tested yet. Started working on tests.... Will work on that a little bit more later tomorrow.

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
+import matpolotlib.pyplot as plt;
+from pylab import rcParams;
+import unittest, sys;
+import random;
+
+def plot_node(node, level = 1, posx = 0, posy = 0):
+    """Graphical Representation of the tree. Perform a DFS travel..."""
+    width = 2000.0 * (0.5**(level))
+    if node._color == 'RED':
+        plt.text(posx, posy, str(node._data), horizontalalignment='center',fontsize=10,color='r');
+    else:
+        plt.text(posx, posy, str(node._data), horizontalalignment='center',fontsize=10);
+    if node.left:
+        px = [posx, posx-width]
+        py = [posy-2, posy-15]
+        plt.plot(px,py,'b-',hold=True)
+        plot_node(node._left, level+1, posx-width, posy-20)
+    if node.right:
+        plot_node(node._right, level+1, posx+width, posy-20)
+        px = [posx, posx+width]
+        py = [posy-2, posy-15]
+        plt.plot(px,py,'b-',hold=True)
+
+def plot_tree(node, figsize=(10,6)):
+    rcParams['figure.figsize'] = figsize
+    fig, ax = plt.subplots()
+    ax.axis('off')
+    plot_node(node)
+    plt.show()
 
 class TreeNode(object):
         """
@@ -18,7 +47,7 @@ class TreeNode(object):
         def _find_grandparent(self):
             """Find the grand parent of a tree node. If grand parent
                 not exist, return None."""
-            if self._parent == None:
+            if self._parent is None:
                 return None
             else:
                 parent_node = self._parent
@@ -27,10 +56,10 @@ class TreeNode(object):
         def _find_uncle(self):
             """Find the uncle of a tree node, namely it's grandparent's other child.
                 If parent is None or no grandparent, return None."""
-            if self._parent == None or self._find_grandparent() == None:
+            if self._parent is None or self._find_grandparent() is None:
                 return None
             else:
-                grandpa = self._find_grandparent(self);
+                grandpa = self._find_grandparent();
                 if self._parent == grandpa._left:
                     return grandpa._right
                 else:
@@ -63,6 +92,62 @@ class TreeNode(object):
                 self._color = color;
             else:
                 print "Typo! Typo! Use only 'RED' or 'BLACK' for color"
+
+        def _rotate_left(self):
+            #Implementation going on... Not sure if needed...
+            #
+            """This function rotate a tree node from right to left
+                    x             y
+                   / \           / \
+                  t  y    ->    x  t
+                    / \        / \
+                   t  t       t  t
+            This can be a useful function called when re-balancing the tree.
+            """
+            if self._right is None:
+                return
+            else:
+                new_head = self._right;
+                self._right = new_head._left;
+                if new_head._left is None:
+                    new_head._left._parent = self;
+                new_head._parent = self._parent;
+                if self._parent is None:
+                    self.tree = new_head; #Not sure how this should go...
+                elif self is self._parent._left:
+                    self._parent._left = new_head;
+                else:
+                    self._parent._right = new_head;
+                new_head._left = self;
+                self._parent = new_head;
+
+        def _rotate_right(self):
+            #Implementation going on... Not sure if needed...
+            #
+            """This function rotate a tree node from left to right
+                    x             y
+                   / \           / \
+                  y  t    ->    t  x
+                 / \              / \
+                t  t             t  t
+            This can be a useful function called when re-balancing the tree.
+            """
+            if self._left is None:
+                pass
+            else:
+                new_head = self._left;
+                self._left = new_head._right;
+                if new_head._right is not None:
+                    new_head._right._parent = self;
+                new_head._parent = new_head._parent;
+                if self._parent is None:
+                    BinarySearchTree.tree = new_head;
+                elif self is self._parent._right:
+                    self._parent._right = new_head;
+                else:
+                    self._parent._left = new_head;
+                new_head._right = self;
+                self._parent = new_head
 
         def traverse_infix(self, result = None):
             if result == None:
@@ -175,7 +260,9 @@ class BinarySearchTree(object):
 
 class RBTree(BinarySearchTree):
     """
-        #TODO:Description
+        Red-Black Tree is a special type of Binary Search Tree that has the ability
+        of balancing itself in order to avoid uneven tree which could result in long
+        computational performance when performing traversal.
     """
     def __init__(self):
         """"""
@@ -189,5 +276,16 @@ class RBTree(BinarySearchTree):
 
     def delet(self,x):
         pass
+
+class TestRBTree(unittest.TestCase):
+    def test_insert_small_random(self):
+        small_random = [];
+        rbt = RBTree();
+        for i in range(10):
+            target = random.random();
+            small_random.append(target)
+            rbt.insert(target);
+        ####NOT FINISHED...
+
 
 
