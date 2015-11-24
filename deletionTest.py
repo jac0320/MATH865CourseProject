f36109ed41e3db0f50fe82974d0e99858aa46802
jac0320 <sitew@clemson.edu>
2015-11-24 13:14:29 -0500

    Verson 0.2

--- deletionTest.py
+++ deletionTest.py
@@ -1,14 +1,12 @@
 import main
 
-
-
 people3 = ['Doug','Bob','Alice','Kathy','Tom','Carol']
-bst3 = RBTree()
+bst3 = main.RBTree()
 for p in people3:
 	bst3.rb_insert(p)
 print bst3.tree.traverse_infix()
-plot_tree(bst3.tree)
+main.plot_tree(bst3.tree)
 bst3.rb_delete('Kathy')
 
 print bst3.tree.traverse_infix()
-plot_tree(bst3.tree)
+main.plot_tree(bst3.tree)
