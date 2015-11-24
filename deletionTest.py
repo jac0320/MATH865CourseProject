1384e0d321a6c7ce62fb1853c03baca1bfccd783
jac0320 <sitew@clemson.edu>
2015-11-24 13:17:50 -0500

    Version 0.2.2

--- deletionTest.py
+++ deletionTest.py
@@ -4,7 +4,7 @@ people3 = ['Doug','Bob','Alice','Kathy','Tom','Carol']
 bst3 = rbTree_main.RBTree()
 for p in people3:
 	bst3.rb_insert(p)
-	
+
 print bst3.tree.traverse_infix()
 rbTree_main.plot_tree(bst3.tree)
 bst3.rb_delete('Kathy')
